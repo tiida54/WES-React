26df8fc9c2deae430ab6dea819f080a4ebd5eb82
BulbCat.David <tiida541108@gmail.com>
2019-04-16 19:23:21 +0800

    npm install
    React init

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <Fragment>
+                <form className="store-selector">
+                    <h2>Enter</h2>
+                </form>
+            </Fragment>
+        )
+    }
+}
+
+export default StorePicker;
