50e8814c39db830faad03236e3c5ec61cc664f80
BulbCat.David <tiida541108@gmail.com>
2019-04-20 23:39:29 +0800

    Update index.js
    Events, Refs and this Binding

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,8 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-// import StorePicker from './components/StorePicker';
-// import App from './components/App';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
 import Router from './components/Router'
 import "./css/style.css";
 
