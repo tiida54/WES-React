085c6d3fedc934e43fb80d303a0a6aeed5ff4bf5
BulbCat.David <tiida541108@gmail.com>
2019-04-21 02:15:26 +0800

    Events, Refs and this Binding
    React chapter - 11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,10 +2,14 @@ import React, { Fragment } from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    goToStore = event => {
+        event.preventDefault();
+        console.log("goToStore : ", this);
+    }
     render() {
         return (
             <Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Enter</h2>
                     <input type="text" required placeholder="Store Name" defaultValue={getFunName()}></input>
                     <button type="submit">Visit Store</button>
